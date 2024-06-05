9815f55b09fe8e2fccdfe12a4eae3ea12c659f7c
zaysverse <zeze@jbnu.ac.kr>
2024-06-05 09:33:19 +0900

    프로젝트 생성

---
+++ src/main/java/com/nhnacademy/minidooraydgateway/MiniDoorayDGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraydgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayDGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayDGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooraydgateway/MiniDoorayDGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooraydgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayDGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
